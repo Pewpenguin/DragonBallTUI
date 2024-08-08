c8d3ae6ae1d634ccad4de8ff80073313469ba4b0
Aryamann <115640537+Pewpenguin@users.noreply.github.com>
2024-08-08 16:21:42 +0530

    minor improvement

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io::prelude::*;
+use std::path::Path;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Episode {
@@ -19,14 +20,20 @@ pub struct Series {
 }
 
 pub fn load_guide_from_file(file_path: &str) -> Result<Vec<Series>, Box<dyn std::error::Error>> {
-    let file_content = fs::read_to_string(file_path)?;
+    let path = Path::new(file_path);
+    if !path.exists() {
+        return Err(From::from(format!("File not found: {}", file_path)));
+    }
+
+    let file_content = fs::read_to_string(path)?;
     let guide: Vec<Series> = serde_json::from_str(&file_content)?;
     Ok(guide)
 }
 
 pub fn save_guide_to_file(guide: &Vec<Series>, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let path = Path::new(file_path);
     let file_content = serde_json::to_string_pretty(&guide)?;
-    let mut file = fs::File::create(file_path)?;
+    let mut file = fs::File::create(path)?;
     file.write_all(file_content.as_bytes())?;
     Ok(())
 }
